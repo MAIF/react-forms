4ffc3626519b77cdc2a8d486e4c9443e235f28bb
Quentin <quentino.vega@gmail.com>
2021-10-29 15:04:22 +0200

    Update pg

--- examples/src/Playground.js
+++ examples/src/Playground.js
@@ -58,9 +58,6 @@ export const Playground = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="https://maif.github.io/react-forms/">Home</a>
-              </li>
-              <li className="nav-item">
                 <a className="nav-link" href="https://github.com/MAIF/react-forms#readme">Documentation</a>
               </li>
               <li className="nav-item">
