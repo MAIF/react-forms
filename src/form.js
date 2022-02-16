2748c25fd38017a6e52e4a2af3165d26cd087c8d
Quentin <quentino.vega@gmail.com>
2022-02-16 11:59:39 +0100

    oops

--- src/form.js
+++ src/form.js
@@ -811,7 +811,7 @@ const NestedForm = ({ schema, flow, parent, inputWrapper, maybeCustomHttpClient,
     return [...acc, { step, visibleStep, entry }]
   }, [])
 
-  const bordered = computedSandF.filter(x => x.visibleStep).length <= 1 && step.label === null;
+  const bordered = computedSandF.filter(x => x.visibleStep).length > 1 && step.label === null;
   return (
     <div className={classNames({ [classes.nestedform__border]: bordered })}>
       {!!step.collapsable && schemaAndFlow.flow.length > 1 && collapsed && <ArrowDown size={30} className={classes.cursor_pointer} style={{ position: 'absolute', top: '5px', left: '-16px' }} stroke-width="3" onClick={() => setCollapsed(!collapsed)} />}
