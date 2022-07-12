cc09c90c8fce3c1d725aaa1855710098587477d0
Benjamin CAVY <benjamin.cavy@gmail.com>
2022-07-12 09:32:37 +0200

    Revert "chore: babel -> typescript & embed modules in bundle"
    This reverts commit 6805264038041d72f2ba4b5eb368dcc232e1daee.

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import babel from '@rollup/plugin-babel'
 import del from 'rollup-plugin-delete'
 import pkg from './package.json'
 import { terser } from 'rollup-plugin-terser'
@@ -6,9 +7,7 @@ import command from 'rollup-plugin-command';
 import scss from 'rollup-plugin-scss'
 import typescript from '@rollup/plugin-typescript';
 import dts from "rollup-plugin-dts";
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from "@rollup/plugin-node-resolve"
-import { visualizer } from "rollup-plugin-visualizer";
+//import { visualizer } from "rollup-plugin-visualizer";
 
 const isDev = process.env.NODE_ENV !== 'production'
 const isYalcActivated = process.env.YALC === 'activated'
@@ -16,15 +15,21 @@ const isYalcActivated = process.env.YALC === 'activated'
 export default [{
     input: pkg.source,
     output: [
-      { file: pkg.main, format: 'esm' }
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'esm' }
     ],
     plugins: [
-      visualizer(),
-      resolve(),
-      commonjs(),
+//      visualizer(),
       del({ targets: ['dist/*', 'lib/*', 'examples/node_modules/@maif/react-forms/lib'], verbose: true }),
       typescript(),
       scss(),
+      babel({
+        exclude: [
+          'docs/**',
+          'node_modules/**'
+        ],
+        babelHelpers: 'bundled'
+      }),
       isDev ? undefined : terser(),
       copy({
         targets: [
@@ -36,7 +41,7 @@ export default [{
         verbose: true,
         hook: 'closeBundle'
       }),
-      isYalcActivated ? command('yalc push --replace') : undefined,
+      isYalcActivated ? command('yalc push --replace') : undefined
     ].filter(f => f),
     external: Object.keys(pkg.peerDependencies || {})
   },
