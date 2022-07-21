259483317f8d2a127d79460c9c5824ee0cdc2388
Benjamin CAVY <benjamin.cavy@gmail.com>
2022-07-21 11:42:02 +0200

    feat: more tests

--- cypress/e2e/typeText.cy.js
+++ cypress/e2e/typeText.cy.js
@@ -29,4 +29,52 @@ describe('type text', () => {
       expect(value.name).to.eq("bar")
     })
   })
+
+  it('should not be displayed if visible is false', () => {
+    cy.setSchema(`{
+      name: {
+        type: "string",
+        visible: false
+      }
+    }`)
+
+    cy.findByRole("textbox", { name: /name/i }).should("not.exist")
+  })
+
+  it('should not allow input when disabled', () => {
+    cy.setSchema(`{
+      name: {
+        type: "string",
+        disabled: true
+      }
+    }`)
+
+    cy.findByRole("textbox", { name: /name/i }).should('have.attr', 'readonly', 'readonly')
+  })
+
+  it('allow label modification', () => {
+    cy.setSchema(`{
+      name: {
+        type: "string",
+        label: "nome"
+      }
+    }`)
+
+    cy.findByRole("textbox", { name: /nome/i }).should('exist')
+  })
+
+  it('should allow specific onChange callback', () => {
+    cy.setSchema(`{
+      name: {
+        type: "string",
+        onChange: ({rawValues, value, setValue}) => {
+          setValue('name', value + "" +value)
+        }
+      }
+    }`)
+    cy.findByRole("textbox", { name: /name/i }).type('a')
+    cy.submitAndCheck(value => {
+      expect(value.name).to.eq("aa")
+    })
+  })
 })
