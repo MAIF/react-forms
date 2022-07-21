f76e2f9369cde9b42c4617f1dacbda33faf04211
Benjamin CAVY <benjamin.cavy@maif.fr>
2022-07-21 15:09:46 +0200

    feat: visible specific type

--- src/schema-types.ts
+++ src/schema-types.ts
@@ -1,21 +1,46 @@
 import React from "react";
 
 interface Schema {
-  [x: string]: SchemaEntry
+  [x: string]: SchemaEntry;
+}
+
+interface TInformation {
+  path: string;
+  index?: number;
+  parent?: TInformation;
 }
 
 interface BaseSchema<OutputType> {
   array?: boolean;
-  placeholder?: string; /* FIXME not all fields... */
-  defaultValue?: any; /* TODO could be more specific*/
+  placeholder?: string /* FIXME not all fields... */;
+  defaultValue?: any /* TODO could be more specific*/;
   help?: string;
-  onChange?: (param: {rawValues: object, value: OutputType, setValue: (p1:any, p2:any, p3:any /*TODO*/) => void}) => void;
+  onChange?: (param: {
+    rawValues: object;
+    value: OutputType;
+    setValue: (p1: any, p2: any, p3: any /*TODO*/) => void;
+  }) => void;
   disabled?: boolean;
   label?: React.ReactNode;
   className?: string;
   style?: object;
-  visible?: boolean | {ref: string, test: (b: any, idx?: number) => boolean};
-  render?: (props: {rawValues: object, value: OutputType, onChange: (newValue: OutputType) => void, error?:any, setValue?: (data: any)/*TODO 2 params ?*/ => void, parent: any}) => JSX.Element
+  visible?:
+    | boolean
+    | ((param: {
+        rawValues: object;
+        informations: TInformation;
+        value: OutputType;
+        error?: any;
+        getValue: (x: string) => any;
+      }) => boolean);
+  render?: (props: {
+    rawValues: object;
+    value: OutputType;
+    onChange: (newValue: OutputType) => void;
+    error?: any;
+    setValue?: (data: any) => /*TODO 2 params ?*/ void;
+    parent: any;
+  }) => JSX.Element;
 }
 
 interface DateTypeSchema extends BaseSchema<Date> {
@@ -24,10 +49,9 @@ interface DateTypeSchema extends BaseSchema<Date> {
   defaultValue?: Date | string;
 }
 
-
 interface FileTypeSchema extends BaseSchema<Array<File>> {
   type: "file";
-  format?: undefined
+  format?: undefined;
 }
 
 interface BooleanTypeSchema extends BaseSchema<boolean> {
@@ -38,7 +62,16 @@ interface BooleanTypeSchema extends BaseSchema<boolean> {
 
 interface StringTypeSchema extends BaseSchema<string> {
   type: "string";
-  format?: "select" | "buttons" | "markdown" | "code" | "singleLineCode" | "textarea" | "email" | "password" | "hidden";
+  format?:
+    | "select"
+    | "buttons"
+    | "markdown"
+    | "code"
+    | "singleLineCode"
+    | "textarea"
+    | "email"
+    | "password"
+    | "hidden";
   defaultValue?: string;
 }
 
@@ -48,7 +81,6 @@ interface NumberTypeSchema extends BaseSchema<string> {
   defaultValue?: number;
 }
 
-
 interface FormatSelectSchema<T> extends BaseSchema<T> {
   /* TODO object with select ? Need a transformer or label key */
   type: "string" | "number" | "object";
@@ -56,24 +88,24 @@ interface FormatSelectSchema<T> extends BaseSchema<T> {
   createOption?: boolean;
   onCreateOption?: (option: object) => void; // TODO specify option style
   isMulti?: boolean;
-  options?: Array<any|{label: string, value: any}>;
+  options?: Array<any | { label: string; value: any }>;
   optionsFrom?: string;
-  transformer?: (value: any) => {label: string, value: any};
+  transformer?: (value: any) => { label: string; value: any };
   defaultValue?: T;
 }
 
 interface StringFormatSelectSchema extends FormatSelectSchema<string> {
-  type: "string"
+  type: "string";
 }
 
 interface NumberFormatSelectSchema extends FormatSelectSchema<number> {
-  type: "number"
+  type: "number";
 }
 
 interface ObjectTypeSchema extends BaseSchema<object> {
   type: "object";
   format: undefined;
-  defaultKeyValue?: Array<{key: string, value: any}>;
+  defaultKeyValue?: Array<{ key: string; value: any }>;
   defaultValue?: object;
 }
 
@@ -81,7 +113,7 @@ interface ObjectFormSchema extends BaseSchema<object> {
   type: "object";
   format: "form";
   schema: Schema;
-  defaultKeyValue?: Array<{key: string, value: any}>
+  defaultKeyValue?: Array<{ key: string; value: any }>;
   defaultValue?: object;
 }
 
@@ -97,7 +129,7 @@ export type SchemaEntry =
   | BooleanTypeSchema
   | NumberTypeSchema
   | NumberFormatSelectSchema
-  | StringFormatSelectSchema
+  | StringFormatSelectSchema;
 //| NumberSelectSchema
 //| StringSelectSchema
 
